01bb737e0faed34b8869c3c39849d47072334fc5
Dev Kumar Singh <sdev06829@gmail.com>
2026-09-03 10:51:23 +0530

    React Series

---
+++ React_Vite/01_first/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import ICard from './components/ICard'
+import './App.css'
+
+function App() {
+   const val = 16;
+   const name = "Dev"
+
+  return (
+  
+     <div>
+      
+      <ICard/>
+     </div>
+
+  
+  )
+}
+
+export default App
---
+++ React_Vite/01_first/src/components/ICard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ICard() {
+    const val = 16;
+    const name = "Dev"
+  return (
+    <div>
+      <h2 style = {{color:'red'}}>ABES ICard </h2>
+     <h2 style = {{color:'red'}}> Welcome to React Using Vite</h2>
+     <h2 style = {{color:'red'}}> Value = {val}</h2>
+     <h2 style = {{color:'red'}} > Name = {name}</h2>
+    
+    </div>
+  )
+}
+
+export default ICard
